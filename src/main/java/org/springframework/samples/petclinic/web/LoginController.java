d65d7fbca2e2fe4916dcd38d422a61c5e90b0283
jiwoo <wjswldn21lis@sookmyung.ac.kr>
2025-10-16 20:26:12 +0900

    fix: http session 추가

--- src/main/java/org/springframework/samples/petclinic/web/LoginController.java
+++ src/main/java/org/springframework/samples/petclinic/web/LoginController.java
@@ -17,15 +17,23 @@ public class LoginController {
     @PostMapping("/login")
     public String processLogin(@RequestParam String username,
                                @RequestParam String password,
+                               HttpSession session,
                                Model model) {
 
         // 간단한 예시 (나중에 DB 연동 가능)
         if ("admin".equals(username) && "1234".equals(password)) {
+            session.setAttribute("loginUser", username);
             return "redirect:/";
         } else {
             model.addAttribute("error", "아이디 또는 비밀번호가 올바르지 않습니다.");
             return "login";
         }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/login";
     }
 }
 
